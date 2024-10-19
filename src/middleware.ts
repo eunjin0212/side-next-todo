7e0c39afb11edb2a106a530306dff58e121d5897
EunJin Kim <eunjin9639@gmail.com>
2024-10-19 20:04:07 +0900

    🌈 style:Formatting middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,14 @@
-import { NextResponse } from "next/server";
-import { auth, BASE_PATH } from "@/auth";
+import { NextResponse } from 'next/server';
+import { auth, BASE_PATH } from '@/auth';
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
 
 export default auth((req) => {
   const reqUrl = new URL(req.url);
-  if (!req.auth && reqUrl?.pathname !== "/") {
+
+  if (!req.auth && reqUrl?.pathname !== '/') {
     return NextResponse.redirect(
       new URL(
         `${BASE_PATH}/signin?callbackUrl=${encodeURIComponent(
