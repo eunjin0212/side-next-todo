6c52213b7a6fc684e40a1e49aaa2d4e404305dac
EunJin Kim <eunjin9639@gmail.com>
2024-10-19 20:05:49 +0900

    🔧 Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-   fontFamily: {
-    'pretendard': ['Pretendard Variable']
-   },
-   colors: {
-    positive: '#60a5fa',
-    negative: '#fb7185',
-    warning: '#fbbf24',
-    success: '#4ade80',
-    primary: '#1e3a8a',
-    disabled: '#d4d4d4',
-    emphasized: '#fef9c3',
-   },
+    fontFamily: {
+      'pretendard': ['Pretendard Variable']
+    },
     extend: {
       colors: {
+        positive: '#60a5fa',
+        negative: '#fb7185',
+        warning: '#fbbf24',
+        success: '#4ade80',
+        primary: '#1e3a8a',
+        disabled: '#d4d4d4',
+        emphasized: '#fef9c3',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
