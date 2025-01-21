e72ba7f3b688ac66ea6bba17c94b03209226934d
EunJin Kim <eunjin9639@gmail.com>
2025-01-21 23:26:33 +0900

    ✨ feat: Enhance Notion API integration by handling TodoList data structure

--- src/app/api/notion/list/route.ts
+++ src/app/api/notion/list/route.ts
@@ -1,7 +1,33 @@
 import { NextResponse } from 'next/server';
-import { Client } from "@notionhq/client";
+import { Client } from '@notionhq/client';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+interface TodoList {
+  checked: boolean;
+  text: string;
+  createTime: string;
+}
+
+interface TodoListResponse {
+  parent: {
+    database_id: string;
+  };
+  properties: {
+    createdAt: {
+      create_time: string;
+    };
+    text: {
+      rich_text: { plain_text: string }[];
+    };
+    id: {
+      title: { plain_text: string }[];
+    };
+    checked: {
+      check: boolean;
+    };
+  };
+}
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
@@ -9,13 +35,24 @@ export async function GET(req: Request) {
 
   try {
     const response = await notion.databases.query({
-      database_id: dbName
+      database_id: dbName,
     });
 
-    return NextResponse.json(response);
+    const handledData: TodoList[] | null = response.results.length
+      ? (response.results as unknown as TodoListResponse[]).map((item) => ({
+        checked: item.properties.checked.check,
+        text: item.properties.text.rich_text[0].plain_text,
+        createTime: item.properties.createdAt.create_time,
+      }))
+      : [];
+
+    return NextResponse.json(handledData);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Failed to query database' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Failed to query database' },
+      { status: 500 }
+    );
   }
 }
 
@@ -39,6 +76,9 @@ export async function POST(req: Request) {
     return NextResponse.json(response);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Failed to query database' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Failed to query database' },
+      { status: 500 }
+    );
   }
 }
